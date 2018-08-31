bcbd4fb0eca8424918c1e68ee4c6cf3c005e0ebc
caseysmithrc <30840394+caseysmithrc@users.noreply.github.com>
2018-08-31 08:30:24 -0600

    Update Invoke-Atomic.ps1

--- Invoke-Atomic.ps1
+++ Invoke-Atomic.ps1
@@ -23,8 +23,7 @@ $SupportedPlatforms = 'windows'
 #Ascii Art 
 
 $AtomicAsciiArt = @'
-
-     ___    __                  _         ____           __   ______
+     ___   __                  _         ____           __   ______
     /   | / /_____  ____ ___  (_)____   / __ \___  ____/ /  /_  __/__  ____ _____ ___
    / /| |/ __/ __ \/ __ `__ \/ / ___/  / /_/ / _ \/ __  /    / / / _ \/ __ `/ __ `__ \
   / ___ / /_/ /_/ / / / / / / / /__   / _, _/  __/ /_/ /    / / /  __/ /_/ / / / / / /
@@ -40,7 +39,7 @@ Write-Host $AtomicAsciiArt -Foreground Green
 
 # Loop Over All Files, Convert Each to Variable Name Of the Test.
 
-$AtomicFilePath = 'C:\Users\subtee\downloads\atomic-red-team-master\atomics\' #Input Parameter Defined in beginning of script.
+$AtomicFilePath = 'C:\Users\subtee\downloads\atomic-red-team-fixed\atomics\' #Input Parameter Defined in beginning of script.
 
 function Set-AtomicArray([string] $AtomicFilePath)
 {
@@ -48,14 +47,12 @@ function Set-AtomicArray([string] $AtomicFilePath)
 	$AtomicTechniqueArray = @()
 	
 	Get-Childitem $AtomicFilePath -Recurse -Filter *.yaml -File | ForEach-Object {
-	#Get-ChildItem -Path $AtomicFilePath -Filter *.yaml -Recurse -File -Name | ForEach-Object {
 		try
 		{
 			$currentTechnique = [System.IO.Path]::GetFileNameWithoutExtension($_.FullName);
 			$parsedYaml = (ConvertFrom-Yaml (Get-Content $_.FullName -Raw ))
 			New-Variable -Name $currentTechnique -Value $parsedYaml -Scope "Script";
-			#We could also return objects here
-			#Returning Objects Allows For Pipeline BEGIN PROCESS FINISH etc..
+			#TODO properly return Objects Allows For Pipeline BEGIN PROCESS FINISH etc..
 			$AtomicTechniqueArray += $parsedYaml
 		}
 		catch
@@ -73,15 +70,15 @@ function ExecuteAtomicTechnique([System.Collections.Hashtable] $AtomicTechnique)
 	#Takes $AtomicTechnique Parsed From YAML and Executes it with Default Values
 	#Designed to Run ALL Tests For A Technique
 	Write-Host "[********BEGIN TEST*******]`n" $AtomicTechnique.display_name.ToString(), $AtomicTechnique.attack_technique.ToString() "has" $AtomicTechnique.atomic_tests.Count "Test(s)"  -Foreground Yellow 
-	#Call ExecuteAtomicTest
-	$AtomicTechnique.atomic_tests | %{ ExecuteAtomicTest $_ }
+	#Call Get-AtomicTest
+	$AtomicTechnique.atomic_tests | %{ Get-AtomicTest $_ }
 	Write-Host "[!!!!!!!!END TEST!!!!!!!]`n"
 	
 }
 
 #Refactor this to Get-AtomicTest and output execution plans
 
-function ExecuteAtomicTest([System.Collections.Hashtable] $AtomicTest)
+function Get-AtomicTest([System.Collections.Hashtable] $AtomicTest)
 {
 	#Short Circuit Exit On Conditions
 	#For Now We Only care about Windows Tests
@@ -96,6 +93,7 @@ function ExecuteAtomicTest([System.Collections.Hashtable] $AtomicTest)
 		
 	if($AtomicTest.input_arguments.Count -gt 0)
 	{
+		#Check Data Type, s/b string. example 1065 is being cast as int
 		$InputArgs = [Array]($AtomicTest.input_arguments.Keys).Split(" ")
 		$InputDefaults = [Array]( $AtomicTest.input_arguments.Values | %{$_.default }).Split(" ")
 		
@@ -106,31 +104,71 @@ function ExecuteAtomicTest([System.Collections.Hashtable] $AtomicTest)
 		}
 		
 		Write-Host $finalCommand -Foreground Green
-		
 	}
 	else
 	{
 		Write-Host $finalCommand -Foreground Green
-		
 	}
 	
 	
 }
 
+function Invoke-AtomicTest ([System.Collections.Hashtable] $AtomicTest)
+{
+	#Short Circuit Exit On Conditions
+	#For Now We Only care about Windows Tests
+	if ( !($AtomicTest.supported_platforms.Contains($SupportedPlatforms)) ){ return }
+	#Reject Manual Tests
+	if ( ($AtomicTest.executor.name.Contains('manual')) ) 	{ return }
+
+	Write-Host $AtomicTest.name.ToString()
+	Write-Host $AtomicTest.description.ToString()
+	
+	$finalCommand = $AtomicTest.executor.command
+		
+	if($AtomicTest.input_arguments.Count -gt 0)
+	{
+		#Check Data Type, s/b string. example 1065 is being cast as int
+		$InputArgs = [Array]($AtomicTest.input_arguments.Keys).Split(" ")
+		$InputDefaults = [Array]( $AtomicTest.input_arguments.Values | %{$_.default }).Split(" ")
+		
+		for($i = 0; $i -lt $InputArgs.Length; $i++)
+		{	
+			$findValue = '#{' + $InputArgs[$i] + '}'
+			$finalCommand = $finalCommand.Replace( $findValue, $InputDefaults[$i] )
+		}
+		
+		#Write-Host $finalCommand -Foreground Green
+	}
+	else
+	{
+		#Write-Host $finalCommand -Foreground Green
+	}
+	
+	$shellcommand = ""
+	if ($AtomicTest.executor.name -eq 'command_prompt') { $shellCommand = "cmd.exe /c $finalCommand"  }
+	if ($AtomicTest.executor.name -eq 'powershell') { $shellCommand = "powershell.exe -iex $finalCommand"  }
+	Write-Host  -Foreground Green
+	
+	Write-Host $shellCommand -Foreground Green
+	iex "& $shellCommand" 
+	
+	
+	
+}
+
+
 function Invoke-ChainReaction([System.Collections.Hashtable] $AtomicTechnique)
 {
 
 	
-	$AtomicTechnique.atomic_tests | %{ ExecuteAtomicTest $_ }
+	$AtomicTechnique.atomic_tests | %{ Get-AtomicTest $_ }
 	
 }
 
 
 $AllAtomicTechniques = Set-AtomicArray([string] $AtomicFilePath) #Sets Up variables, Adjust Method to be one, some, or all...
 $AllAtomicTechniques.Count 
-
-#Executes ALL Tests
-$AllAtomicTechniques | %{ ExecuteAtomicTechnique $_ }
 
 
 
@@ -227,13 +265,21 @@ o
 
 
 
+<# Sample ChainReaction 
 
 $ChainReaction = @($T1117, $T1118, $T1086 )
 $ChainReaction | % { Invoke-ChainReaction $_ }
 Write-Host $AsciiChainReacion -Foreground Cyan
 
+#>
 
+#Generates ALL Tests!
+$AllAtomicTechniques | %{ ExecuteAtomicTechnique $_ }
 
+# Executes a Single Technique, All Tests
+# $T1117.atomic_tests | %{ Invoke-AtomicTest $_ } 
+# Executes a Single Technique, Single Test
+$T1117.atomic_tests[1] | %{ Invoke-AtomicTest $_ } 
 
 $PowerShellBoom = @'
      _.-^^---....,,--       
@@ -252,6 +298,6 @@ $PowerShellBoom = @'
 
  
  
-Write-Host $PowerShellBoom -Foreground Yellow
+#Write-Host $PowerShellBoom -Foreground Yellow
 Write-Host "Test Complete, Go Sift Through The Fallout" -Foreground Cyan
 
